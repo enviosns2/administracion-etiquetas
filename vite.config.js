61c57419c08ce62be5cc6d668845129a3a31c8b0
Diego <enviosns2@gmail.com>
2025-01-23 14:46:19 -0600

    Optimización del build con copyfiles y ajustes en configuración

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        // Dividir las dependencias principales en chunks separados
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'axios'], // Chunks para librerías comunes
+          pdf: ['jspdf'], // Chunks para jsPDF
+          barcode: ['jsbarcode'], // Chunks para jsBarcode
+        },
+      },
+    },
+    chunkSizeWarningLimit: 500, // Ajustar el límite de advertencia
+  },
+});
